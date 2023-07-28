79b7fbc03feb808f0babbb9385de0767d1582b60
acorey81 <acorey81@gmail.com>
2023-07-28 09:37:47 -0700

    adding port

--- server.js
+++ server.js
@@ -102,6 +102,6 @@ app.get('/profile/:id', (req, res) => {
         .catch(err => res.status(400).json('unable to get entries'))
       })
 
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('app is running on port {process.env.PORT}');
 })
